c73296c09973f43f24bf4b4ae62682cba0908997
teena92084 <103328068+teena92084@users.noreply.github.com>
2022-06-24 16:21:55 +0530

    Add files via upload

---
+++ use dump.py
@@ -0,0 +1,24 @@
+# import json
+# a={"teena":"teena","age":20,"city":"bhilwara"}
+# file=open("my datels.json","w")
+# c=json.dump(a,file)
+# print(c)
+
+
+
+
+
+##we are use dump mathod for write file #
+
+
+
+# import json
+# name={"teena":"bhagwat","kajal":"tina","reena":"meena"}
+# cv=open("name store","w")
+# json.dump(name,cv)
+
+# import json
+# a={"teena":"teena","age":20,"city":"bhilwara"}
+# file=open("my datels.json","w")
+# json.dump(a,file)
+
